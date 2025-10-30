c4d0c404298bc4a32b46f5034dc53159fee8b62a
Joao Vitor <joao.jvts@gmail.com>
2025-10-29 21:33:14 -0300

    desafio mestre

--- main.c
+++ main.c
@@ -4,41 +4,58 @@ void movendoCavalo()
 {
   int i = 0;
   printf("Movento do cavalo\n");
-  while (i < 1)
+  while (1 == 1)
   {
 
     for (int j = 0; j < 2; j++)
     {
       printf("baixo\n");
     }
-    i++;
     printf("esquerda\n");
+    i++;
+    if (i == 1)
+    {
+      break;
+    }
+  }
+}
+
+void movimento(int n, char *direcao)
+{
+  printf("%s\n", direcao);
+  if (n > 1)
+  {
+    movimento(n - 1, direcao);
+  }
+}
+
+void movimentoBispo(int n)
+{
+  for (int i = 0; i < 1; i++)
+  {
+    printf("direita\n");
+    for (int j = 0; j < 1; j++)
+    {
+      printf("cima\n");
+      if (n > 1)
+      {
+        movimentoBispo(n - 1);
+      }
+    }
   }
 }
 
 int main()
 {
   printf("Movendo torre para direita\n");
-  for (int i = 0; i < 5; i++)
-  {
-    printf("Direita\n");
-  }
+  movimento(5, "direita");
 
   printf("Movendo bispo na diagonal\n");
-  int i = 0;
-  while (i < 2)
-  {
-    i++;
-    printf("Cima, Direita\n");
-  }
+  movimentoBispo(2);
 
   printf("Movendo rainha para esquerda\n");
-  int j = 0;
-  do
-  {
-    printf("Esquerda\n");
-    j++;
-  } while (j < 8);
+  movimento(8, "Esquerda");
+
   movendoCavalo();
 
   return 0;
